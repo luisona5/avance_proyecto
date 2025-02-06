5e06681589ee056a9f68820feca3bcbbb6009103
luisona5 <150484099+luisona5@users.noreply.github.com>
2025-02-06 04:13:34 -0500

    conexion

---
+++ src/main/java/org/example/LOGIN.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LOGIN {
+    private JPasswordField contrasena;
+    private JTextField usuario;
+    private JButton ingresarButton;
+    public JPanel login;
+
+
+    public LOGIN() {
+        ingresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,18 @@
 
+import org.example.LOGIN;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        UsuarioDAO dao = new UsuarioDAO();
-
-        // Insertar usuario
-        dao.insertarUsuario("Ana Gómez", "ana@gmail.com", 30);
-
-        // Leer usuarios
-        dao.leerUsuarios();
-
-        // Actualizar usuario (reemplaza el ID con un valor real de tu colección)
-        // Usa el ID generado automáticamente en MongoDB
-        dao.actualizarUsuario("id_generado", "Ana Actualizada", "ana_actualizada@gmail.com", 31);
-
-        // Leer usuarios nuevamente
-        dao.leerUsuarios();
-
-        // Eliminar usuario (reemplaza el ID con un valor real de tu colección)
-        dao.eliminarUsuario("id_generado");
-
-        // Leer usuarios nuevamente
-        dao.leerUsuarios();
-
+        JFrame frame = new JFrame("Login | Radio Button Use");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 600);
+        frame.setPreferredSize(new Dimension(600, 600));
+        frame.setContentPane(new LOGIN().login);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
